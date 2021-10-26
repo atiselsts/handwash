f84a98a04700ea00c24a34b75fc36aa2e2fe58f9
Atis Elsts <atis.elsts@gmail.com>
2021-10-26 18:00:40 +0300

    update the optical flow generation script

--- optical-flow.py
+++ optical-flow.py
@@ -4,11 +4,16 @@ import os
 import cv2 as cv
 import numpy as np
 
-input_dir = "kaggle-dataset-6classes-minimal"
-output_dir = "kaggle-dataset-6classes-of"
+input_dir = "../kaggle-dataset-6classes"
+output_dir = "../kaggle-dataset-6classes-of"
 
 N_CLASSES = 7
-classes = [str(i) in i in range(N_CLASSES)]
+classes = [str(i) for i in range(N_CLASSES)]
+
+FPS = 30
+
+# what step to use for movements?
+frame_step = FPS // 3
 
 def mk(filename):
     try:
@@ -16,74 +21,69 @@ def mk(filename):
     except Exception as ex:
         pass
 
-# The following frees up resources and
-# closes all windows
-#cap.release()
-#cv.destroyAllWindows()
 
-def extract_flow(c, filename):
-    in_fullname = os.path.join(input_dir, c, filename)
-    cap = cv.VideoCapture(in_fullname)
-    frame_num = 0
+def read_frames(video_filename):
+    cap = cv.VideoCapture(video_filename)
 
-    out_fullname = os.path.join(output_dir, c, "frame_{}_" + filename + ".jpg")
-
+    frames = []
     ret, frame = cap.read()
 
-    # Converts frame to grayscale because we
-    # only need the luminance channel for
-    # detecting edges - less computationally
-    # expensive
-    prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+    while ret:
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frames.append(gray)
+        ret, frame = cap.read()
 
-    # Creates an image filled with zero
-    # intensities with the same dimensions
-    # as the frame
-    mask = np.zeros_like(frame)
+    cap.release()
+    return frames
+
+
+def frame_sequence_to_flow(f1, f2):
+    # Calculates dense optical flow by Farneback method
+    flow = cv.calcOpticalFlowFarneback(f1, f2,
+                                       None,
+                                       0.5, 3, 15, 3, 5, 1.2, 0)
+
+    # Computes the magnitude and angle of the 2D vectors
+    magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
+
+    # Creates an image filled with zero intensities with the same dimensions as the frame
+    mask = np.zeros((len(f1), len(f1[0]), 3), dtype=np.float32)
+
+    # Sets image hue according to the optical flow
+    # direction
+    mask[..., 0] = angle * 180 / np.pi / 2
 
     # Sets image saturation to maximum
     mask[..., 1] = 255
 
-    while ret:
-	
-	# Converts each frame to grayscale - we previously
-	# only converted the first frame to grayscale
-	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-	
-	# Calculates dense optical flow by Farneback method
-	flow = cv.calcOpticalFlowFarneback(prev_gray, gray,
-					   None,
-					   0.5, 3, 15, 3, 5, 1.2, 0)
-	
-	# Computes the magnitude and angle of the 2D vectors
-	magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
-	
-	# Sets image hue according to the optical flow
-	# direction
-	mask[..., 0] = angle * 180 / np.pi / 2
-	
-	# Sets image value according to the optical flow
-	# magnitude (normalized)
-	mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
-	
-	# Converts HSV to RGB (BGR) color representation
-	rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
-	
-	# Opens a new window and displays the output frame
-	#cv.imshow("dense optical flow", rgb)
-	
-	# Updates previous frame
-	prev_gray = gray
+    # Sets image value according to the optical flow
+    # magnitude (normalized)
+    mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
 
-        save_path_and_name = out_fullname.format(frame_num)
-        frame_num += 1
-        cv2.imwrite(save_path_and_name, rbg)
-        print("saved", save_path_and_name)
+    # Converts HSV to RGB (BGR) color representation
+    rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
 
-        # read next frame
-	ret, frame = cap.read()
+    return rgb
 
-    cap.release()
+
+def extract_flow(c, filename):
+    in_fullname = os.path.join(input_dir, c, filename)
+    out_fullname = os.path.join(output_dir, c, "frame_{}_" + os.path.splitext(filename)[0] + ".jpg")
+
+    print("processing", in_fullname)
+    frames = read_frames(in_fullname)
+
+    n = len(frames)
+    flow_frame_num = 0
+    for i in range(n - frame_step):
+        frame1 = frames[i]
+        frame2 = frames[i + frame_step]
+        flow_frame = frame_sequence_to_flow(frame1, frame2)
+
+        save_path_and_name = out_fullname.format(flow_frame_num)
+        flow_frame_num += 1
+        cv.imwrite(save_path_and_name, flow_frame)
+        #print("saved", save_path_and_name)
 
 
 def main():
@@ -91,12 +91,10 @@ def main():
     for c in classes:
         mk(os.path.join(output_dir, c))
 
-    for c in classes:        
+    for c in classes:
         for filename in os.listdir(os.path.join(input_dir, c)):
             if filename[-4:] == ".mp4":
                 extract_flow(c, filename)
-        for filename in os.listdir(dirname):
-        fullname = os.path.join(dirname, filename)
 
 
 if __name__ == "__main__":
